f1943f049e2567eefefc387fda8f8c558ef7ff99
Diego Antonio Gomez Diaz <diegogomez211990@gmail.com>
2025-03-16 11:49:28 -0400

    Update settings.lua

--- lua/diegoagd/core/settings.lua
+++ lua/diegoagd/core/settings.lua
@@ -1,73 +1,31 @@
--- Set highlight on search
-vim.o.incsearch = true
-vim.o.hlsearch = false
+vim.opt.guicursor = ""
 
--- Make line numbers default
-vim.o.number = true
-vim.o.relativenumber = true
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
--- Enable mouse mode
-vim.o.mouse = "a"
-
--- Sync clipboard between OS and Neovim.
-vim.o.clipboard = "unnamedplus"
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = "yes"
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone,noselect"
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
--- " update vim after file update from outside
-vim.opt.autoread = true
-
--- " Indentation
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.smarttab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-
--- " Always use spaces insted of tabs
 vim.opt.expandtab = true
 
--- " Don't wrap lines
-vim.opt.wrap = true
--- " Wrap lines at convenient points
-vim.opt.linebreak = true
--- " Show line breaks
-vim.opt.showbreak = "↳"
+vim.opt.smartindent = true
 
--- " Start scrolling when we'are 8 lines aways from borders
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
 vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 15
-vim.opt.sidescroll = 5
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 
--- " This makes vim act like all other editors, buffers can
--- " exist in the background without being in a window.
-vim.opt.hidden = true
-
--- " Add the g flag to search/replace by default
-vim.opt.gdefault = true
-
--- Lazy redraw
-vim.o.lazyredraw = true
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+vim.g.mapleader = " "
